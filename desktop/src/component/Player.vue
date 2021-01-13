81629bb71f06c23971d57971da81eff960ff45e3
WildWind <wildwind3325@gmail.com>
2021-01-13 22:01:45 +0800

    Add video.js

---
+++ desktop/src/component/Player.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="div-center">
+    Player
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Player',
+  props: {
+    actived: Boolean
+  },
+  data() {
+    return {};
+  },
+  mounted() { }
+};
+</script>
