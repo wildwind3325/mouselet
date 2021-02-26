09c901985b032939a0242e450b77a8f0f17cf51d
张俊 <junz02@hascovision.com>
2021-02-26 15:46:20 +0800

    eh

--- desktop/src/component/EH.vue
+++ desktop/src/component/EH.vue
@@ -1,16 +1,39 @@
 <template>
   <div>
     <Form :label-width="80">
-      <FormItem label="Account">
-        <Input type="text" v-model="account" placeholder="Account">
+      <FormItem label="Path">
+        <Input type="text" v-model="save_path" :disabled="working">
         </Input>
       </FormItem>
-      <FormItem label="Password">
-        <Input type="password" v-model="password" placeholder="Password">
+      <FormItem label="Url">
+        <Input v-model="url" :disabled="working">
         </Input>
+      </FormItem>
+      <FormItem label="From">
+        <Input v-model="from" :disabled="working">
+        </Input>
+      </FormItem>
+      <FormItem label="To">
+        <Input v-model="to" :disabled="working">
+        </Input>
+      </FormItem>
+      <FormItem label="Indexing">
+        <i-switch v-model="indexing" :disabled="working" />
+      </FormItem>
+      <FormItem label="Medium">
+        <i-switch v-model="medium" :disabled="working" />
       </FormItem>
       <FormItem>
-        <Button type="primary">Login</Button>
+        <Button type="primary" :loading="working" @click="download">Download</Button>
+        <Button :disabled="!working" style="margin-left: 5px;" @click="cancel">Cancel</Button>
+        <Button type="warning" :loading="working" style="margin-left: 5px;" @click="resume">Resume</Button>
+        <Button type="info" :loading="working" style="margin-left: 5px;" @click="login">Login</Button>
+      </FormItem>
+      <FormItem label="Progress">
+        <Progress :percent="0" :status="status" />
+      </FormItem>
+      <FormItem label="Message">
+        {{ message }}
       </FormItem>
     </Form>
   </div>
@@ -25,9 +48,30 @@ export default {
   },
   data() {
     return {
-      account: '',
-      password: ''
+      save_path: '',
+      url: '',
+      from: '',
+      to: '',
+      indexing: false,
+      medium: false,
+      working: false,
+      status: 'active',
+      message: 'Ready'
     };
+  },
+  methods: {
+    download() {
+      this.working = true;
+    },
+    cancel() {
+      this.working = false;
+    },
+    resume() {
+      this.working = true;
+    },
+    login() {
+      this.working = true;
+    }
   }
 };
 </script>
